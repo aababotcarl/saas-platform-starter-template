3f6e89ec50c038d1ac83cde98c8d6e6c1fc5c131
csfinch <carl@aababot.com>
2020-02-21 11:30:51 -0500

    Updating Pricing Plans

--- Controllers/home.js
+++ Controllers/home.js
@@ -1,3 +1,12 @@
+require('dotenv').config();
+const stripe = require('stripe')('sk_test_8rK2xXnFgnkVJX9yRZ3VVVwW');
+const STRIPE_API = require('../api/stripe-functions.js');
 exports.index = async(req, res) => {
-   res.render('home/index.ejs');
+   STRIPE_API.getAllProductsAndPlans().then(products => {
+      products = products.filter(product => {
+        return product.plans.length > 0;
+      });
+  
+      res.render('home/index.ejs', {products: products});
+    });
 };
