830255dc33e0d155066e2af4c2ce75d357e205e4
gocsfinch <carl@aababot.com>
2021-05-13 18:30:02 -0400

    Updated dashboard template

--- Routes/index.js
+++ Routes/index.js
@@ -2,10 +2,10 @@ var homeCtrl = require('../Controllers/home');
 var accountCtrl = require('../Controllers/account');
 module.exports = function(app, passport){
     
-    app.get('/', homeCtrl.home);
+    app.get('/', accountCtrl.dashboard);
     app.get('/account/signup', accountCtrl.signup);
     app.get('/account/signin', accountCtrl.signin);
-    app.get('/account/dashboard', isAuthenticated, accountCtrl.dashboard);
+    // app.get('/account/dashboard', isAuthenticated, accountCtrl.dashboard);
     app.post('/account/signin', passport.authenticate('local-signin', {successRedirect: '/account/dashboard', failureRedirect: '/account/signin'}));
     app.post('/account/signup', passport.authenticate('local-signup', {successRedirect: '/account/dashboard', failureRedirect: '/account/signup'}));
     app.get('/account/signout', accountCtrl.signout);
--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set('view engine', 'ejs');
 
 //set ejs laytouts
 app.use(expressLayouts);
+app.set('layout', 'layouts/dashboard');
 
 //models for the database
 var models = require('./Models');
@@ -30,14 +31,14 @@ var routes = require('./Routes/index.js')(app, passport);
 require('./config/passport.js')(passport, models.User);
 
 //sync db
-models.sequelize.sync().then(function(){
+/* models.sequelize.sync().then(function(){
 	console.log("Database setup!");
 }).catch(function(err){
 	console.log(err);
-});
+}); */
 
 //run app
-app.listen(3000, function(err){
+app.listen(5000, function(err){
 	if(!err)
 		console.log('app is working correctly.');
 	console.log(err);
